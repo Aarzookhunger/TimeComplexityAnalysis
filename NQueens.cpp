a16b57c657147455e666d9edd6fb20b8792a07c2
Aarzoo Khunger <116306749+Aarzookhunger@users.noreply.github.com>
2024-04-18 09:38:45 +0530

    Add files via upload

---
+++ NQueens.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std ;
+class NQueen
+{
+  int n ;
+  vector<int> x ;
+  public :
+  NQueen(int n)
+  {
+    this->n = n ;
+    x.resize(n+1,0) ;
+  }
+  bool place(int k , int i)
+  {
+    for(int j=1; j<=k-1 ; j++)
+    {
+      if(x[j]==i || abs(j-k)==abs(x[j]-i))
+      return false; 
+    }
+    return true ;
+  }
+  void NQ(int k)
+  {
+    for(int i=1; i<=n ; i++)
+    {
+      if(place(k,i))
+      {
+        x[k]=i ;
+        if(k==n)
+        {
+          for(int i=1 ; i<=n ; i++)
+          {
+            for(int j=1 ; j<=n ; j++)
+            {
+              if(x[i]==j) cout << " Q "  ;
+              else cout << " * " ;
+            }
+            cout << endl ;
+          }
+          cout << endl;
+          return ;
+        }
+        else NQ(k+1);
+      }
+    }
+  }
+};
+int main()
+{
+  int n;
+  cout << "Enter number of queens :- " ; cin >> n ;
+  NQueen N(n); 
+  N.NQ(1) ;
+}
